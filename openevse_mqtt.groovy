5dec6d159fe4b0ac35fc6e9c8a72972cd1f4d5db
remsg <78101963+remsg@users.noreply.github.com>
2023-05-11 15:26:00 -0700

    Added event throttling
    Added even throttling to limit number of events generated.  Worked around an odd issue where device.currentValue may sometimes return null

--- openevse_mqtt.groovy
+++ openevse_mqtt.groovy
@@ -51,6 +51,7 @@ metadata {
         attribute "voltage", "number"
         attribute "override", "string"
         attribute "temperature", "number"
+
 preferences {
     section("Settings for connection from HE to Broker") {
         input name: "topicName", type: "text", title: "OpenEVSE Name(Topic name)", required: true
@@ -64,14 +65,18 @@ preferences {
         input name: "watchDogSched", type: "bool", title: "Check for connection to MQTT broker on a schedule?", defaultValue: false, required: true
         input name: "watchDogTime", type: "number", title: "This number of minutes to check for connection to MQTT broker", defaultValue: 15, range: "1..59", required: true
         input name: "logLevel",title: "IDE logging level",multiple: false,required: true,type: "enum", options: getLogLevels(), submitOnChange : false, defaultValue : "1"
-        input name: "ShowAllPreferences", type: "bool", title: "<b>Show OpenEVSE device settings?</b>These change settings on the device itself through MQTT", defaultValue: false
-        }
-   if( ShowAllPreferences ){
-    section("Settings for OpenEVSE"){
+//        input name: "ShowAllPreferences", type: "bool", title: "<b>Show OpenEVSE device settings?</b>These change settings on the device itself through MQTT", defaultValue: false
+//        }
+//   if( ShowAllPreferences ){
+//    section("Settings for OpenEVSE"){
         // put configuration here
-        if (topicName && ipAddr && ipPort) input ( "max_current", "number", title: "<b>Max Current for Manual Override</b> (6-80, default 24)", defaultValue: 24,range: "6..80", required: false)
-        if (topicName && ipAddr && ipPort) input ( "charge_current", "number", title: "<b>Charge Current for Manual Override</b> (6-80, default 24)", defaultValue: 24,range: "6..80", required: false)
-        if (topicName && ipAddr && ipPort) input ( "auto_release", "bool", title: "<b>Auto-release when the vehicle is disconnected, false if manual override will persist after vehicle disconnection.</b> (true/false, default true)", defaultValue: 'true', required: false)
+        input name: "throttle", type: "number", title: "Throttle stats to update every n sec unless charging", defaultValue: 60, range: "30..900", required: true
+        input name: "throttleExtras", type: "bool", title: "Throttle temp events even while charging", defaultValue: true, required: true
+        input name: "tempUnit", type: "bool", title: "Temperature in F", defaultValue: true, required: true
+        input ( "max_current", "number", title: "<b>Max Current for Manual Override</b> (6-80, default 24)", defaultValue: 24,range: "6..80", required: false)
+        input ( "charge_current", "number", title: "<b>Charge Current for Manual Override</b> (6-80, default 24)", defaultValue: 24,range: "6..80", required: false)
+        input ( "auto_release", "bool", title: "<b>Auto-release when the vehicle is disconnected, false if manual override will persist after vehicle disconnection.</b> (true/false, default true)", defaultValue: 'true', required: false)
+//        if (topicName && ipAddr && ipPort) input ( "auto_release", "bool", title: "<b>Auto-release when the vehicle is disconnected, false if manual override will persist after vehicle disconnection.</b> (true/false, default true)", defaultValue: 'true', required: false)
         }
    }
     /*
@@ -80,8 +85,7 @@ preferences {
         input(name: "logLevel",title: "IDE logging level",multiple: false,required: true,type: "enum", options: getLogLevels(), submitOnChange : false, defaultValue : "1")
         }
     */
-}
-}
+    }
 }
 
 def setVersion(){
@@ -111,38 +115,71 @@ void parse(String description) {
 
     def value=interfaces.mqtt.parseMessage(description).payload
     if (value) {
+//        debuglog "Topic ${topic[1]} arrived with value ${value}"
         updateTopic(topic[1], value)
     } else {
         debuglog "Empty payload: \"" + value + "\""
     }
 }
 void updateTopic(topic, value) {
+    currentValue = state.lastValue[topic]
 
-    if (value != device.currentValue(topic)) {
+    if (currentValue != null) {
+        if ( !(topic.equals("state") && value == 3) || !(topic.equals("amp") && value == 0)) {
+            value = throttleUpdate(topic, value, currentValue)
+        }
+        if ( throttleExtras ) {
+            switch(topic) {
+                case "temp2":
+                    value = throttleUpdate(topic, value, currentValue)
+                case "temp4":
+                    value = throttleUpdate(topic, value, currentValue)
+            }
+        }
+    }
 
-        debuglog "Topic: " + topic + " Value: " + value + " Current value: " + device.currentValue(topic)
+    if (value != currentValue) {
         sendEvent(name: topic, value: value)
+        state.lastValue[topic] = value
+        debuglog "Topic: " + topic + " Value: \"" + value + "\" Current value: \"" + currentValue + "\""
         switch(topic) {
             case "amp":
                 sendEvent(name: "amperage", value:  (null != value) ? (value).toInteger() / 1000 : "n/a")
             case "temp2":
-                sendEvent(name: "temperature", value:  (null != value) ? (value).toInteger() / 10 : "n/a")
+                value = (null != value) ? value.toInteger() / 10 : "n/a"
+                temp = tempUnit ? celsiusToFahrenheit(value) : value
+                sendEvent(name: "temperature", value: temp)
             case "vehicle":
                 def presenceMap = ["1":"present", "0":"not present"]
                 sendEvent(name: "presence", value:  (null != value) ? presenceMap[value] : "n/a")
             case "divertmode":
                 def divertMap = ["1":"Normal", "2":"Eco"]
-                debuglog "Divert Mode" + divertMap[value]
                 sendEvent(name: "Divert", value: (null != value) ? divertMap[value] : "n/a")
         }
     }
+}
+
+def throttleUpdate(topic, value, lastValue) {
+    timenow = now()
+    state.lastUpdate[topic] = (null == state.lastUpdate[topic]) ? 1 : state.lastUpdate[topic]
+    delta = timenow - state.lastUpdate[topic]
+ //   debuglog "${topic} last update before: ${state.lastUpdate[topic]} now ${timenow} delta: ${delta} threshold: ${state.throttle_ms} incoming value: ${value} previous value: ${lastValue}"
+    if(timenow - state.lastUpdate[topic] <= state.throttle_ms) {
+        debuglog "Throttled ${topic}, returning previous value ${lastValue}"
+        return lastValue
+    }
+    state.lastUpdate[topic] = timenow
+//    debuglog "Last update now: ${state.lastUpdate[topic]} value is now ${value}"
+    return value
 }
 
 void updated() {
     infolog "updated..."
     //set schedules
     unschedule()
+    state.clear()
     pauseExecution(1000)
+    initializeLastUpdate()
     //schedule the watchdog to run in case the broker restarts
     if (watchDogSched) {
         debuglog "setting schedule to check for MQTT broker connection every ${watchDogTime} minutes"
@@ -153,6 +190,7 @@ void updated() {
         debuglog "setting schedule to refresh every ${refreshTime} minutes"
         schedule("22 3/${refreshTime} * ? * *", requestStatus)
     }
+    state.throttle_ms = throttle * 1000
 }
 void uninstalled() {
     infolog "disconnecting from mqtt..."
@@ -163,6 +201,7 @@ void uninstalled() {
 def divertMode( mode )
 {
     watchDog()
+    initializeLastUpdate()
     def divertMap = ["Normal":"1", "Eco":"2"]
     infolog "setting divert mode to ${mode} ${divertMap[mode]}"
     interfaces.mqtt.publish("${topicName}/divertmode", divertMap[mode])
@@ -170,6 +209,7 @@ def divertMode( mode )
 def enableManualOverride( Ostate, Ocharge_current, Omax_current, Oauto_release )
 {
     watchDog()
+    initializeLastUpdate()
     def overrideOptions = [:]
     overrideOptions.state = Ostate
     overrideOptions.charge_current = (null != Ocharge_current) ? Ocharge_current.toString() : charge_current.toString()
@@ -183,6 +223,7 @@ def enableManualOverride( Ostate, Ocharge_current, Omax_current, Oauto_release )
 def toggleManualOverride()
 {
     watchDog()
+    initializeLastUpdate()
     infolog "toggling manual override"
     interfaces.mqtt.publish("${topicName}/override/set", "toggle")
 }
@@ -190,6 +231,7 @@ def toggleManualOverride()
 def clearManualOverride()
 {
     watchDog()
+    initializeLastUpdate()
     infolog "clearing manual override"
     interfaces.mqtt.publish("${topicName}/override/set", "clear")
 }
@@ -197,6 +239,7 @@ def clearManualOverride()
 def on()
 {
     watchDog()
+    initializeLastUpdate()
     def overrideOptions = [:]
     overrideOptions.state = 'active'
     overrideOptions.charge_current = charge_current.toString()
@@ -210,6 +253,7 @@ def on()
 def off()
 {
     watchDog()
+    initializeLastUpdate()
     def overrideOptions = [:]
     overrideOptions.state = 'disabled'
     overrideOptions.charge_current = charge_current.toString()
@@ -237,7 +281,7 @@ void initialize() {
         //give it a chance to start
         pauseExecution(1000)
         infolog "connection established..."
-        def topicList = [
+        topicList = [
             "override",    // Override Status {state:active/disabled, charge_current:>=0, max_current:>=0, auto_release:true/false}
             "pilot",        // Charge current allowed in Amps
             "divertmode",    // Divert Mode 1-Normal, 2-Eco (Solar) Divert
@@ -256,6 +300,8 @@ void initialize() {
             debuglog "Subscribing to: ${topicName}/${it}"
             mqttInt.subscribe("${topicName}/${it}")
         }
+        initializeLastUpdate()
+        state.throttle_ms = throttle * 1000
 
     } catch(e) {
         log.warn "${device.label?device.label:device.name}: MQTT initialize error: ${e.message}"
@@ -265,6 +311,16 @@ void initialize() {
     if (logL == 2) runIn(3600,logsOff)
 }
 
+def initializeLastUpdate() {
+    state.lastUpdate = [:]
+    state.lastValue = [:]
+    topicList.each {
+        state.lastUpdate[it] = 1
+        state.lastValue[it] = null
+        debuglog "initializing ${it} to ${state.lastUpdate[it]} and ${state.lastValue[it]}"
+    }
+}
+
 def watchDog() {
     debuglog "Checking MQTT status"
     //if not connnected, re-initialize
@@ -272,6 +328,7 @@ def watchDog() {
         debuglog "MQTT Connected: (${interfaces.mqtt.isConnected()})"
         initialize()
     }
+    state.throttle_ms = throttle * 1000
 }
 void mqttClientStatus(String message) {
 	log.warn "${device.label?device.label:device.name}: **** Received status message: ${message} ****"
